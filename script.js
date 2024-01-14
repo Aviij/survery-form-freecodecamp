6e0ac67dd746ecc51e433feca5bf7550360bbb3c
Aviij <avishkadd9@gmail.com>
2024-01-14 21:16:08 +0530

    project completed

---
+++ script.js
@@ -0,0 +1,71 @@
+
+console.time("timer");
+// trying to do some form validation here with chatGPT, but I understood nothing 14.JAN
+/*
+document.addEventListener('DOMContentLoaded', function(){
+
+    const form = document.getElementById('survey-form');
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        //validation logic will go here
+
+        vlaidateInput('name');
+    });
+
+    //validate Input 
+    function vlaidateInput(inputId){
+
+        const inputElement = document.getElementById(inputId);
+        const inputValue  = inputElement.value.trim();
+
+        if (inputValue === ''){
+            displayError(inputElement,'${inputId} is required! ');
+        }else {
+            removeError(inputElement);
+        }
+
+
+    }
+
+    function displayError(element, message){
+        const errorElement = document.createElement('div');
+        errorElement.className = 'error-message';
+        errorElement.textContent = message;
+
+        element.parentNode.appendChild(errorElement);
+    }
+    function removeError(){
+        element.parentNode.querySelector('.error-message');
+
+        if(errorElement){
+            errorElement.remove();
+        }
+    }
+
+)}
+*/
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.timeEnd("timer");
